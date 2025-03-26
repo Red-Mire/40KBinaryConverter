66ab8d9bb6be3225b4f2e8ba899fdecd6f51c7a9
Red-Mire <appleremy@gmail.com>
2025-03-26 12:49:52 +0100

    First iteration of the main menu and basic menu navigation

--- Menu.cs
+++ Menu.cs
@@ -2,5 +2,39 @@ using System;
 
 public class Menu
 {
-    Console.Writeline("aaa");
+    
+    public void DisplayMainMenu()
+    {
+        string menu = @"
+==== Welcome to 40K Binary Converter ====
+
+1. Binary to string
+2. String to binary
+3. Exit
+
+Choose menu number (1-3)
+
+==== Glory to the Machine God! ====
+";
+        
+        Console.WriteLine(menu);
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                break;
+
+            case "2":
+                break;
+
+            case"3": //Fermeture du programme
+                Console.Clear();
+                Console.WriteLine("Praise the Omnissiah!");
+                Environment.Exit(0);
+                break;
+        }
+    }
+    
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main() //Méthode Main qui lance le programme
+    {
+        Console.ForegroundColor = ConsoleColor.Green; //Changement de la couleur de police de la console au lancement du programme
+        
+        Menu menu = new Menu();
+        menu.DisplayMainMenu();
+    }
+}
